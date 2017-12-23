f020f4e796f6a11529c34cf933a77af18c9bb410
AyatDiab <32824151+AyatDiab@users.noreply.github.com>
2017-12-22 22:54:19 -0600

    Add files via upload

---
+++ prog3Pentago_adiab4.c
@@ -0,0 +1,28 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio 15
+VisualStudioVersion = 15.0.26228.10
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "prog3Pentago_adiab4_hkhan49.c", "prog3Pentago_adiab4_hkhan49.c\prog3Pentago_adiab4_hkhan49.c.vcxproj", "{992EB3B3-8192-4013-BCA5-92AE49A5A51B}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{992EB3B3-8192-4013-BCA5-92AE49A5A51B}.Debug|x64.ActiveCfg = Debug|x64
+		{992EB3B3-8192-4013-BCA5-92AE49A5A51B}.Debug|x64.Build.0 = Debug|x64
+		{992EB3B3-8192-4013-BCA5-92AE49A5A51B}.Debug|x86.ActiveCfg = Debug|Win32
+		{992EB3B3-8192-4013-BCA5-92AE49A5A51B}.Debug|x86.Build.0 = Debug|Win32
+		{992EB3B3-8192-4013-BCA5-92AE49A5A51B}.Release|x64.ActiveCfg = Release|x64
+		{992EB3B3-8192-4013-BCA5-92AE49A5A51B}.Release|x64.Build.0 = Release|x64
+		{992EB3B3-8192-4013-BCA5-92AE49A5A51B}.Release|x86.ActiveCfg = Release|Win32
+		{992EB3B3-8192-4013-BCA5-92AE49A5A51B}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+EndGlobal
